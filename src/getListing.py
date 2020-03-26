4fcf9dba323251c70f46811ee506e1eadcf4e03c
Thomas Fay <thomas.fay@esss.se>
2020-03-26 21:13:27 +0100

    WIP

--- src/getListing.py
+++ src/getListing.py
@@ -4,64 +4,68 @@ import sys
 import time
 import os
 import getopt
-import re
-
+import argparse
+"""
+"""
 
 def usage():
-  print("usage: " + sys.argv[0] + " [options] ")
-  print("Get a listing of all nodes underneath a top-level node that match a node pattern")
-  print("Input arguments are ALL REQUIRED. No arguments are optional")
+  print("usage: " + sys.argv[0] + " [options]")
+  print("Get a listing of FBS nodes contained underneath a particular FBS branch as descendant relationship.")
+  print("E.g. show nodes contained within System X.")
+  print("Exlusion of node patterns and specification of the number of nested levels to return are supported.")
   print("")
-  print("  -i --inFilse: JSON file containing the pre-filtered FBS nodes relevant to this FBS branch.")
-  print("  -f --fbsPrefix: FBS prefix to use as top-level node. Leading '=' is optional.")
-  print("  -m --match: Match arbitrary FBS node pattern e.g. KF01 or KF or WG. Lists are supported as comma separated value inputs, see example below")
+  print("  -i --inFile:     JSON file containing the pre-filtered FBS nodes relevant to this FBS branch.")
+  print("  -f --fbsPrefix:  OPTIONAL (default to root node) FBS prefix to use as top-level node.")
+  print("  -e --exclude:    OPTIONAL (default exclude none) FBS nodes to exclude from listing, empty string for no exlusions, supports list format as comma separated values.")
+  print("  -l --levels:     OPTIONAL (default levels = 1) Number of levels to show results for. Integers < 1 means show all available levels.")
   print("")
-  print("e.g. " + sys.argv[0] + " --inFile rfq.json --fbsPrefix ESS.ACC.A01 --match WG")
-  print("e.g. " + sys.argv[0] + " --inFile rfq.json --fbsPrefix ESS.ACC.A01 --match WG,WH,WG")
+  print("e.g. " + sys.argv[0] + " --inFile=rfq.json --fbsPrefix=ESS.ACC.A01.E01 --exclude '' --levels 1")
+  print("e.g. " + sys.argv[0] + " --inFile=rfq.json --fbsPrefix=ESS.ACC.A01.E01 --exclude WG,W --levels 2")
   sys.exit(1)
 
+gnuOptions=["inFile=", "fbsPrefix=", "exclude=", "levels="]
+parser = argparse.ArgumentParser()
+parser.add_argument('--inFile')
+parser.add_argument('--fbsPrefix')
+parser.add_argument('--exclude')
+parser.add_argument('--levels', type=int, help='Number of levels to show results for. Default is 1')
 
-unixOptions="i:f:m:"
-gnuOptions=["inFile=", "fbsPrefix=", "match="]
+args = parser.parse_args()
+inFile = args.inFile
+fbsPrefix = args.fbsPrefix
+exclude = args.exclude
+levels = args.levels
 
-try:
-  optionList, arguments = getopt.getopt(sys.argv[1:], unixOptions, gnuOptions)
-except getopt.error as err:
-  usage()
+if exclude is None:
+  exclude="ZZZZ"
 
-inFile=""
-fbsPrefix=""
-match=""
+if levels is None:
+  levels = 1
+elif levels < 1:
+  levels = 50
 
-for option, argument in optionList:
-  if option in ("-i", "--inFile") or option[:3] == "--i":
-    inFile = argument
-  elif option in ("-f", "--fbsPrefix") or option[:3] == "--f":
-    fbsPrefix = argument
-  elif option in ("-m", "--match") or option[:3] == "--m":
-    matchNode = argument
 
-if len(inFile) < 1 or len(fbsPrefix) < 1 or len(matchNode) < 1:
-  usage()
-
-temp=matchNode.split(',')
-list_matchNodes = list()
-if len(temp) == 0:
-  print("Invalid match definition, exiting...")
-  exit(2)
-elif len(temp) == 1:
-  list_matchNodes.append(matchNode)
+list_exclude = list()
+temp = exclude.split(',')
+if len(temp) == 1 and not temp[0].isalpha():
+  list_exclude.append("ZZZZ")
+elif len(temp) == 1 and temp[0].isalpha():
+  list_exclude.append(temp[0])
 else:
-  for el in temp:
-    list_matchNodes.append(el)
+  list_exclude=list(temp)
 
-# match arbitary FBS nodes BUT limit substring search to four chars
-for matchNode in list_matchNodes:
-  if len(matchNode) > 4:
-    print("FBS nodes have maximum 4 characters, exiting...")
-    exit(2)
+if inFile is None:
+  usage()
+
+print(list_exclude)
 
 fPath = os.path.dirname(os.path.realpath(__file__))
+with open(fPath + "/../json/" + inFile) as inputFile:
+  list_FBS=json.load(inputFile)
+
+if fbsPrefix is None:
+  fbsPrefix=list_FBS[0]['tag']
+
 # Allow lazy prescription of fbsPrefix 
 # And autofill any missing leading or trailing char.
 if not fbsPrefix.endswith("."):
@@ -69,57 +73,36 @@ if not fbsPrefix.endswith("."):
 if not fbsPrefix.startswith("="):
   fbsPrefix="=" + fbsPrefix
 
+#list_matchedNodes(Tag, Description)
+list_childNodes = list()
+
+# Parse the FBS for matching nodes
+for el in list_FBS:
+  noClash = 0
+  tagFull = el['tag']
+  tag = tagFull.replace(fbsPrefix,'')
+  if fbsPrefix in tagFull:
+    for excluded in list_exclude:
+      if excluded not in tag:
+        noClash += 1
+    if noClash == len(list_exclude):
+      if tagFull.count('.') < (fbsPrefix.count('.') + levels):
+        list_childNodes.append([tagFull,el['description']])
+
+list_output = list()
+
 midBranch = "├── "
 endBranch = "└── "
 
-with open(fPath + "/../json/" + inFile) as inputFile:
-  list_FBS=json.load(inputFile)
-
-#list_matchedNodes(Tag, Description)
-list_matchedNodes = list()
-
-# Parse the FBS for matching nodes
-for matchNode in list_matchNodes:
-  for el in list_FBS:
-    tag = el['tag']
-    if fbsPrefix in tag:
-      # Match against last branch of tree (the "leaf node").
-      leafNode = tag.split('.')[-1]
-      leafNodeComponent = re.sub("[0-9]", "", leafNode)
-      if matchNode == leafNodeComponent:
-        list_matchedNodes.append([el['tag'],el['description']])
-
-  # Recurse the FBS for ancestor nodes of each matched leaf node.
-  list_output = list()
-
-  for leaf in list_matchedNodes:
-    leafTag = str(leaf[0])
-    level = leafTag.count('.') - fbsPrefix.count('.')
-    # Find ancestor information (tag + description)
-    # Interested in parent relationships only here.
-
-    while level > 0:
-      foundParent = False
-      parentTag = leafTag[:leafTag.rfind('.')]
-      for node in list_FBS:
-        tag = node['tag']
-        if parentTag == tag:
-          leafTag = parentTag
-          level-=1
-          foundParent = True
-          if [tag, node['description']] not in list_matchedNodes: 
-            list_matchedNodes.append([tag, node['description']])
-          break
-      if not foundParent:
-        print("Can't find parent node for tag: " + leafTag)
-
-
-list_matchedNodes.sort()
-list_output = list()
-
-for el in list_matchedNodes:
+for el in list_childNodes:
   list_output.append(midBranch + el[0] +  " ( " + el[1] + " )")
 
+if len(list_output) <1:
+  print("No matches found.")
+  exit(0)
+
+
+list_output.sort()
 list_output[-1]=list_output[-1].replace(midBranch,endBranch)
 
 for el in list_FBS:
