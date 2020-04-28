dc6b87f3e8510fec0f2bdb2f67788ea6221cf9ef
Thomas Fay <thomas.fay@esss.se>
2020-04-28 09:38:42 +0200

    Refines field fetching script.

--- src/getField.py
+++ src/getField.py
@@ -16,32 +16,32 @@ def usage():
   print("")
   print("  -i --inFile:     JSON file containing the pre-filtered FBS nodes relevant to this FBS branch.")
   print("  -f --fbsPrefix:  OPTIONAL (default to root node) FBS prefix to use as top-level node.")
-  print("  -t --type: OPTIONAL (default to ID) type of field to match. Valid options: id, essName, parent, modified, state, cableName, description")
+  print("  -f --field: OPTIONAL (default to ID) Field to match. Valid options: all, id, essName, parent, level, modified, state, cableName, description")
   print("")
   print("e.g. " + sys.argv[0] + " --inFile=rfq.json --fbsPrefix=ESS.ACC.A01.E01")
-  print("e.g. " + sys.argv[0] + " --inFile=rfq.json --fbsPrefix=ESS.ACC.A01.E01")
+  print("e.g. " + sys.argv[0] + " --inFile=rfq.json --fbsPrefix=ESS.ACC.A01.E01 --field id")
   sys.exit(1)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--inFile')
 parser.add_argument('--fbsPrefix')
-parser.add_argument('--type')
+parser.add_argument('--field')
 
 args = parser.parse_args()
 inFile = args.inFile
 fbsPrefix = args.fbsPrefix
-type = args.type
+field = args.field
 
 #Check inputs
-if type is None:
-  type = 'id'
+if field is None:
+  field = 'all'
 if inFile is None:
   usage()
 
-validTypes = ['id', 'essName', 'modified', 'state', 'cableName', 'description']
+validFields = ['all', 'id', 'essName', 'modified', 'parent', 'level', 'state', 'cableName', 'description']
 
-if type not in validTypes:
-  print("type is invalid")
+if field not in validFields:
+  print("field is invalid")
   exit(1)
 
 fPath = os.path.dirname(os.path.realpath(__file__))
@@ -49,7 +49,7 @@ with open(fPath + "/../json/" + inFile) as inputFile:
   list_FBS=json.load(inputFile)
 
 if fbsPrefix is None:
-  fbsPrefix=list_FBS[0][type]
+  fbsPrefix=list_FBS[0][field]
 
 # Allow lazy prescription of fbsPrefix 
 # And autofill any missing leading character.
@@ -65,8 +65,18 @@ for el in list_FBS:
   tagFull = el['tag']
   tag = tagFull.replace(fbsPrefix,'')
   if fbsPrefix == tagFull:
-    print(el[type])
-    exit(0)
+    if field != 'all':
+      print(el[field])
+      exit(0)
+    else:
+      for iter in validFields:
+        if iter != 'all':
+          if el[iter] is None:
+            value = 'null'
+          else:
+            value = str(el[iter])
+          print(iter + ": " + value)
+      exit(0)
 
 print("no match found")
 exit(1)
