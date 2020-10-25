83f460760dbc9391b5e4820a02923425c61d02dd
=thomasfay <=thomas.fay@esss.se>
2020-10-25 23:00:58 +0100

    Adds all fields to peek functionality.

--- src/peek.py
+++ src/peek.py
@@ -10,21 +10,24 @@ parser = argparse.ArgumentParser()
 parser.add_argument('inFile', help = 'The input JSON formatted file containing the ESS breakdown structure to parse.')
 parser.add_argument('--offset', type=int, help = 'Peek into file with offset n from first node.')
 parser.add_argument('--rangeNodes', help ='Peek into file with offset n1 to n2. e.g. \'0,5\'')
+parser.add_argument('--all', nargs ='?', const = True, default = None,  help = 'Show all fields')
 args = parser.parse_args()
 inFile = args.inFile
 offset = args.offset
 rangeNodes = args.rangeNodes
+all = args.all
 
 if offset is None:
     offset = 0
 
-n1 = None
-n2 = None
+n1 = offset
+n2 = offset + 1
 
 if rangeNodes is not None:
     temp = rangeNodes.split(',')
     n1 = int(temp[0])
-    n2 = int(temp[1])
+    n2 = int(temp[1]) + 1
+
 
 fPath = os.path.dirname(os.path.realpath(__file__))
 with open(fPath + "/../json/" + inFile) as inputFile:
@@ -33,10 +36,13 @@ lenNodes = len(listBreakdown)
 validKeys = listBreakdown[0].keys()
 nNodes = "{:,}".format(lenNodes)
 print(nNodes + " nodes.")
-if rangeNodes is None:
-    print("Node " + str(offset) + ": " + listBreakdown[offset]['tag'] + " (" + listBreakdown[offset]['description'] + ")" )
 
-if n1 is not None and n2 is not None:
-    for offsetRange in range(n1,n2):
-        print("Node " + str(offsetRange) + ": " + listBreakdown[offsetRange]['tag'] + " (" + listBreakdown[offsetRange]['description'] + ")" )
-
+for offsetRange in range(n1,n2):
+    print("Node " + str(offsetRange) + ": " + listBreakdown[offsetRange]['tag'] + " (" + listBreakdown[offsetRange]['description'] + ")" )
+    if all:
+        for el in validKeys:
+            fieldVal = listBreakdown[offsetRange][el] 
+            if fieldVal is not None:
+                print("-> " + el + ": " + str(fieldVal))
+            else:
+                print("-> " + el + ": " + "None")
