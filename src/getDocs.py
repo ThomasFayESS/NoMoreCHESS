0dfc776f942220c42d0f0816e97eb1b823764f45
Thomas Fay <thomas.fay@esss.se>
2020-11-12 11:36:26 +0100

    Add download functionality to getDocs script.

--- src/getDocs.py
+++ src/getDocs.py
@@ -1,48 +1,71 @@
 import xml.etree.ElementTree as ET
 import argparse
+import subprocess
+import shlex
+# Collection of local helper functions
+import helpers
 
 parser = argparse.ArgumentParser()
 parser.add_argument('inFile', help ='Input file in XML format.')
 parser.add_argument('tag', help = 'FBS tag to get document listing for.')
+parser.add_argument('--download', nargs='?', const = True, default = None, help = 'Download all the documents found for this node.')
+parser.add_argument('--docTag', help = 'Match only this document tag.')
 
 args = parser.parse_args()
 inFile = args.inFile
 findTag = args.tag
+download = args.download
+docTag = args.docTag
 
 fpath='../xml/'
 inFile = 'fbs'
 tree = ET.parse(inFile)
 
 root = tree.getroot()
-title,doc_id,link,filename = '','','',''
+
+allDocTags=["chess_number", "doc_filename", "doc_link" ,"doc_title", "link_id"]
+
+if docTag is None:
+    # We will use CHESS number as a title grouping for document tag values
+    displayDocTags=allDocTags[1:]
+else:
+    if docTag in allDocTags:
+        displayDocTags = docTag
+    else:
+        displayDocTags = allDocTags[1:]
 
 for childRoot in root:
     if childRoot.attrib['tag'] == findTag:
         print("%s ( %s )" % (childRoot.attrib['tag'],childRoot.attrib['description']))
-        elDocuments = childRoot[-1]
-        if elDocuments.tag == "documents":
-            if len(elDocuments) == 0:
+        Documents = childRoot[-1]
+        if Documents.tag == "documents":
+            if len(Documents) == 0:
                 print("No documents attached to this node.")
-            for doc in elDocuments:
+            for doc in Documents:
+                # Found a document is attached to this node
                 if doc.tag == 'document':
-                    for elDoc in doc:
-                        if elDoc.tag =='chess_number':
-                            if elDoc.text is not None:
-                                id = elDoc.text
-                            else:
-                                id = 'No ID found.'
-                            print(id)
-                        elif elDoc.tag == 'doc_title':
-                            title = elDoc.text
-                            if elDoc.text is not None:
-                                title = elDoc.text
-                            else:
-                                title = 'No title found' 
-                        elif elDoc.tag == 'doc_filename':
-                            if elDoc.text is not None:
-                                filename = elDoc.text
-                            else:
-                                filename = 'No filename found'
-                    print(" -> title: " + title)
-                    print(" -> filename: " + filename)
-            exit()
+                    link_id =''
+                    doc_filename = ''
+                    chess_number = ''
+                    for DocField in doc:
+                        if DocField.tag == 'chess_number':
+                            chess_number = DocField.text
+                            if download is None:
+                                print(chess_number)
+                        if DocField.tag == 'link_id':
+                            link_id = DocField.text
+                        if DocField.tag == 'doc_filename':
+                            doc_filename = DocField.text
+                    if download is None:
+                        for DocField in doc:
+                            if DocField.tag in displayDocTags:
+                                print("-> %s: %s" % (DocField.tag,DocField.text))
+                    elif download:
+                        if link_id is not None and doc_filename is not None:
+                            wgetArgs = helpers.wgetArgs
+                            wgetArgs[-3] = wgetArgs[-3].replace("{{ LINK_ID }}", '21308.51166.14592.56101')
+                            wgetArgs[-1] = wgetArgs[-1].replace("{{ FILENAME }}", doc_filename) 
+                            print("Downloading %s " % doc_filename)
+                            subprocess.run(wgetArgs, capture_output=True)
+                        else:
+                            print("No meta-data for %s. Cannot download." % chess_number) 
--- src/helpers.py
+++ src/helpers.py
@@ -39,3 +39,13 @@ def getRoot(str):
     if str[-1] == '$':
         strOut += '.' + temp[-1]
     return strOut
+
+wgetArgs=['wget', '--header', 'Host: chess.esss.lu.se',
+'--user-agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:68.0) Gecko/20100101 Firefox/68.0',
+'--header', 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'--header', 'Accept-Language: en-GB,en;q=0.5',
+'--referer', 'https://chess.esss.lu.se/enovia/ess/webdav/essWebdavIntegrationFrame.jsp',
+'--header', 'Cookie: JSESSIONID=365C3A9A6C158112C24706F918A965C0.instance_E1; RBACCookie=YTgzN2E0MjYtOTZkMi0zZDM5LTllODEtMTgxMmUwNTIzOGFj; _ga=GA1.2.363358576.1601474201; afs=41979825-ce7d-422e-a341-68982f3d4d60; _gid=GA1.2.1173947252.1605003065; swymlang=en',
+'--header', 'Upgrade-Insecure-Requests: 1',
+'https://chess.esss.lu.se/enovia/tvc-action/essDocumentDownload?versionObjectId={{ LINK_ID }}&inline=false',
+'--output-document', '../docs/{{ FILENAME }}']
